056310bcd9bfbde6dce348d30d6b4c6b1c388c95
MaxMarkovA <max.markov@mail.ru>
2023-01-24 21:24:24 +0300

    Constructed function layout which will later serve for directory content analysis

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # Max Markov 01.24.2023
 
+from __future__ import annotations
+from typing import Optional
+from dataclasses import dataclass
+
 from argparse import ArgumentParser
 import logging
-from dataclasses import dataclass
+import hashlib
 
 
 PROGRAM_NAME = 'Directory Profiler'  # TODO here script name is required
@@ -47,16 +51,55 @@ class MessageWriter:
 @dataclass
 class Directory:
     """Directory-related collected information"""
+    name: str
+    parent: Optional[Directory]
 
 
 @dataclass
 class File:
     """File-related collected information"""
+    name: str
+    last_modified: float
+    access_rules: int
+    content_hash: hashlib.sha256
+    directory: Directory
+
+
+def recursive_directory_walker(root_directory: str, max_depth: int):
+    """Performs bypass through directory content & generating file/directory locations
+    NOTE: Based on 'Breadth first search' algorithm - see https://en.wikipedia.org/wiki/Breadth-first_search
+    :param root_directory: Absolute path to starting directory for an algorithm
+    :param max_depth: Recursion limit representing maximum directory stacking
+    """
+
+
+def directory_file_system_handler(path: str):
+    """For each element in directory checks if it is a directory or a file & calls sufficient data collector
+    :param path: Absolute path to processed file system element
+    :return: Gathered information about directory elements
+    """
+
+
+def file_data_collector(file_path: str, directory: Directory) -> File:
+    """Gathers data about specified file & creates File from it
+    :param file_path: Absolute path for file system element proven to be a file
+    :param directory: Directory which this file is stored in
+    :return: File object based on gathered data
+    """
+
+
+def directory_data_collector(directory_path: str, parent: Directory = None) -> File:
+    """Gathers data about specified directory & creates Directory from it
+    :param directory_path: Absolute path for file system element proven to be a directory
+    :param parent: Parent Directory
+    :return: File object based on gathered data
+    """
 
 
 def main():
     """Doin' Stuff..."""
     arguments, not_parsed = ConsoleArgumentParser().parse_known_args()
+    # TODO on next usage translate into a dictionary vars(arguments)
 
 
 if __name__ == '__main__':
