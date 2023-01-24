fe5a07dae7cfad083fc43966ca5c605d676dc977
MaxMarkovA <max.markov@mail.ru>
2023-01-24 03:17:44 +0300

    Added tests.py which contains functional tests

---
+++ tests.py
@@ -0,0 +1,12 @@
+# Max Markov 01.24.2023
+
+import unittest
+
+
+class NormalTestCase(unittest.TestCase):
+    def test_fail(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
